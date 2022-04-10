80462c158e568002081c134a95d28bb5e7697bf7
matvey <aevorobiov@gmail.com>
2022-04-10 22:34:38 +0300

    вынесли фукнцию смены местоположения отностильно дверей (switch_rooms), вынесена логика перехода transition_to_the_room

--- door.py
+++ door.py
@@ -15,6 +15,18 @@ class Door:
     def getRoom(self):
         return self.rooms[1]
 
+    def switch_rooms(self, text):
+        print(text)
+        self.rooms = [self.rooms[1], self.rooms[0]]
+
+    def transition_to_the_room(self, pers):
+        if self.rooms[1].death == True:
+            self.switch_rooms('Вы умерли') # to be continue
+            return self.rooms[0]
+        else:
+            self.switch_rooms(f'Вы перешли в комнату {self.getRoom().name}')
+            return self.rooms[0]
+
     # # @later
     def enter(self, pers):
         if self.key:
@@ -22,20 +34,11 @@ class Door:
                 inin = i.lower().split()
                 if ('ключ' in inin):
                     if inin[3].lower() == self.name.lower():
-                        if pers.get_room().death == True:
-                            print('Вы умерли')
-                        else:
-                            print('Вы перешли в комнату ', self.getRoom().name)
-                            room_ret = self.getRoom()
-                            self.rooms = [self.rooms[1], self.rooms[0]]
-                            return room_ret
+                        return self.transition_to_the_room(pers)
             print('Эту дверь не так легко открыть к ней нужен ключ')
             return self.rooms[0]
         else:
-            print('Вы перешли в комнату ', self.getRoom().name)
-            room_ret = self.getRoom()
-            self.rooms = [self.rooms[1], self.rooms[0]]
-            return room_ret
+            return self.transition_to_the_room(pers)
 
     #     def enter(self, pers):
     #         if self.key == True:
